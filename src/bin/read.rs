4f56a96154e6caebc01ac198d7b5a08422517221
omkar-mohanty <franzohouser@gmail.com>
2022-11-12 14:26:01 +0530

    added reading image functionality

--- src/bin/read.rs
+++ src/bin/read.rs
@@ -1,4 +1,10 @@
 use opencv_wasm::imgcodecs::imread;
+use std::env;
 fn main() {
-    imread("./", 1);
+    let args: Vec<String> = env::args().collect();
+
+    let filename = String::from(args[1].clone());
+    if let Err(msg) = imread(filename, 1) {
+        println!("{}", msg)
+    }
 }

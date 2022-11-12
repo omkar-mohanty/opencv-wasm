b5dcc59543eaf2ef1855b6389c68be2df551bccf
omkar-mohanty <franzohouser@gmail.com>
2022-11-12 14:24:56 +0530

    compiled static libs using wasi-sdk

--- lib/test.sh
+++
@@ -1,2 +0,0 @@
-wasicc libopencv_calib3d.a libopencv_flann.a libopencv_imgproc.a libopencv_stitching.a libopencv_core.a libopencv_gapi.a libopencv_ml.a libopencv_ts.a libopencv_dnn.a  libopencv_highgui.a libopencv_objdetect.a libopencv_video.a libopencv_features2d.a libopencv_imgcodecs.a libopencv_photo.a libopencv_videoio.a -o opencv.wasm -Wl,--no-entry
-
